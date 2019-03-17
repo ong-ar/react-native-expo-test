b9b4deec4c6e8378505968de3ef668923bf811f4
ong-ar <reclusis@gmail.com>
2019-03-17 16:01:31 +0900

    navigation practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,92 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { createAppContainer, createBottomTabNavigator, createStackNavigator, NavigationScreenProp } from 'react-navigation'; // Version can be specified in package.json
 
-export default () => (
-  <View style={{ marginTop: 50 }}>
-    <Text>Hello Expo!</Text>
-  </View>
+interface IProps {
+  navigation: NavigationScreenProp<any, any>;
+}
+
+class HomeScreen extends React.Component<IProps> {
+  public render() {
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  public render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends React.Component<IProps> {
+  public render() {
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('ProfileDetails')}
+        />
+      </View>
+    );
+  }
+}
+
+class ProfileDetailsScreen extends React.Component {
+  public render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const ProfileStack = createStackNavigator(
+  {
+    ProfileDetails: ProfileDetailsScreen,
+    Profile: ProfileScreen,
+  },
+  {
+    initialRouteName: 'Profile',
+  },
 );
+
+const HomeStack = createStackNavigator(
+  {
+    Details: DetailsScreen,
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: HomeStack,
+    Profile: ProfileStack,
+  },
+);
+
+const AppContainer = createAppContainer(TabNavigator);
+
+export default class App extends React.Component {
+  public render() {
+    return <AppContainer />;
+  }
+}
